2f7dc76192edd9ccbca16c8e5c997a21a671232e
v_zzkzzhang <v_zzkzzhang@tencent.com>
2021-06-23 21:28:40 +0800

    6-day: 450, 452, 1005

---
+++ algorithm/arrayRela/1005_largestSumAfterKNegations_test.go
@@ -0,0 +1,62 @@
+package arrayRela
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+// 每次排序后，仅针对数组的最小值进行取反
+// 共计执行上面操作 k 次
+func largestSumAfterKNegations(nums []int, k int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	for i := 0; i < k; i++ {
+		sort.Ints(nums)
+		nums[0] = -nums[0]
+	}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
+
+// 注意，取反操作不能放到 if else 的上面来统一处理！
+func largestSumAfterKNegations2(nums []int, k int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	sum := 0
+	sort.Ints(nums)
+	// 如果数组的元素都是大于等于 0 的，那就仅针对第一个值做 k 次取反操作
+	if nums[0] >= 0 {
+		for i := 0; i < k; i++ {
+			nums[0] = ^nums[0] + 1
+		}
+	} else {
+		for i := 0; i < k; i++ {
+			if nums[0] >= 0 {
+				// 按位取反再加一就是这个数的相反数
+				nums[0] = ^nums[0] + 1
+				continue
+			} else {
+				nums[0] = ^nums[0] + 1
+				sort.Ints(nums)
+			}
+		}
+	}
+
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
+
+func TestLargestSumAfterKNegation(t *testing.T) {
+	nums := []int{3, -1, 0, 2}  // k = 3, sum =
+	//nums := []int{4, 2, 3} // k = 1sum = 5
+	//nums := []int{2, -3, -1, 5, -4} // 2
+	fmt.Println(largestSumAfterKNegations2(nums, 3))
+}
--- algorithm/treeRela/450_deleteNode_test.go
+++ algorithm/treeRela/450_deleteNode_test.go
@@ -101,43 +101,43 @@ func deleteNode(root *TreeNode, key int) *TreeNode {
 		// 方法二：用其左子树的最大值替换自身
 		// 交换节点，而不再是交换节点的值！
 		node := root.Right
-		// 找到右子树最小值的父节点
-		for node.Left != nil && node.Left.Left != nil {
-			node = node.Left
-		}
-		// 将最小值的左右子树设置为 root 原本的左右子树
-		node.Left.Left, node.Left.Right = root.Left, root.Right
-		// 更改 root 指针指向的节点
-		root = node.Left
-		// 将原本右子树最小值的节点从原本的右子树中删除
-		node.Left = nil
-		
-		// 上面代码和这里注释掉的内容区别可以看下面的示例
 		// 将 root 的左子树接到 root 右子树的最小值下
 		// 将 root 替换为原本 root 的右子树
-		// for node.Left != nil {
-		// 	node = node.Left
-		// }
-		// node.Left, root = root.Left, root.Right
+		for node.Left != nil {
+			node = node.Left
+		}
+		node.Left, root = root.Left, root.Right
 	}
 	return root
 }
 
+//  示例一：
 //            15
 //    10               20
 // 5      13        18
 //      12   14
+// 示例二：
+//            5
+//     3               6
+// 2       4               7
 func TestDeleteNode(t *testing.T) {
-	bst := &TreeNode{Val: 15,
-		Left: &TreeNode{Val: 10,
-			Left: &TreeNode{Val: 5},
-			Right: &TreeNode{Val: 13,
-				Left:  &TreeNode{Val: 12,Left: &TreeNode{Val: 11}},
-				Right: &TreeNode{Val: 14}}},
-		Right: &TreeNode{Val: 20,
-			Left: &TreeNode{Val: 18}},
+	//bst := &TreeNode{Val: 15,
+	//	Left: &TreeNode{Val: 10,
+	//		Left: &TreeNode{Val: 5},
+	//		Right: &TreeNode{Val: 13,
+	//			Left:  &TreeNode{Val: 12,Left: &TreeNode{Val: 11}},
+	//			Right: &TreeNode{Val: 14}}},
+	//	Right: &TreeNode{Val: 20,
+	//		Left: &TreeNode{Val: 18}},
+	//}
+	bst := &TreeNode{Val: 5,
+		Left: &TreeNode{Val: 3,
+			Left:  &TreeNode{Val: 2},
+			Right: &TreeNode{Val: 4}},
+		Right: &TreeNode{Val: 6,
+			Right: &TreeNode{Val: 7}},
 	}
-	root := deleteNode(bst, 10)
+	root := deleteNode(bst, 3)
 	treeArr := LevelOrder(root)
 	for _, treeLevelArr := range treeArr {
 		fmt.Println(treeLevelArr)
