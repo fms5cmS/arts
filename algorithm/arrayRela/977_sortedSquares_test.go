32d3b14c0e6f3fc56c505376ae1b70955310f55c
fms5cmS <fms5cms@163.com>
2022-07-10 21:56:47 +0800

    array: 977

--- algorithm/arrayRela/977_sortedSquares_test.go
+++ algorithm/arrayRela/977_sortedSquares_test.go
@@ -1,20 +1,3 @@
 package arrayRela
 
-func sortedSquares(nums []int) []int {
-	result := make([]int, len(nums))
-	k := len(nums) - 1
-	// 双指针法，结果数组 result 的最大值在原数组 nums 的两端，要么最左，要么最右
-	// 注：i <= j ！因为最后要处理两个元素
-	for i, j := 0, len(nums); i <= j; {
-		if nums[i]*nums[i] < nums[j]*nums[j] {
-			result[k] = nums[j] * nums[j]
-			j--
-		} else {
-			result[k] = nums[i] * nums[i]
-
-			i++
-		}
-		k--
-	}
-	return result
-}
+// see ../twoPointer/977_sortedSquares_test.gp
