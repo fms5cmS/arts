0e40797d257c3bca0aa8084a43e73aabf3522fd2
fms5cmS <fms5cms@163.com>
2022-11-02 10:39:10 +0800

    add common method for test

--- algorithm/linkedlistRela/common.go
+++ algorithm/linkedlistRela/common.go
@@ -21,3 +21,25 @@ func (node *ListNode) String() string {
 	str.WriteString(strconv.Itoa(cur.Val))
 	return str.String()
 }
+
+func generateListViaArray(array []int) *ListNode {
+	if len(array) < 1 {
+		return nil
+	}
+	head := &ListNode{Val: array[0]}
+	cur := head
+	for i := 1; i < len(array); i++ {
+		cur.Next = &ListNode{Val: array[i]}
+		cur = cur.Next
+	}
+	return head
+}
+
+func generateArrayViaList(head *ListNode) []int {
+	array := make([]int, 0)
+	cur := head
+	for ; cur != nil; cur = cur.Next {
+		array = append(array, cur.Val)
+	}
+	return array
+}
