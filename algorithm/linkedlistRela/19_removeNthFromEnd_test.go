5b8e4e9717a1c5c779c02ab8d97e931c8cfefa0b
fms5cmS <fms5cms@163.com>
2022-11-02 14:58:42 +0800

    add test for 19

--- algorithm/linkedlistRela/19_removeNthFromEnd_test.go
+++ algorithm/linkedlistRela/19_removeNthFromEnd_test.go
@@ -1,10 +1,11 @@
 package linkedlistRela
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+// n < len(head)
 // 假设链表长度为 N，倒数第 n 个节点的索引其实为 N-n！N 个节点从 head 到 tail 共需要 N-1 步。
 // 1. 快指针从 head 先出发，每次走一步，先走 n 步；
 //   - PS：此时快指针距离走到尾节点还有 (N-1)-n 步
@@ -12,17 +13,17 @@ import (
 //   - PS：此时慢指针走了 (N-1)-n 步，索引为 (N-1)-n，正好是 N-n 的前一位
 // 3. 此时慢指针所在位置为要删除节点的前一个节点
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	dummy := &ListNode{Val: 0, Next: head}
+	dummy := &ListNode{Next: head}
 	slow, fast := dummy, dummy
-	for n > 0 && fast != nil {
+	for i := 0; i < n; i++ {
 		fast = fast.Next
-		n--
 	}
-	fast = fast.Next
-	for fast != nil {
+	for fast.Next != nil {
 		slow, fast = slow.Next, fast.Next
 	}
-	slow.Next = slow.Next.Next
+	tmp := slow.Next
+	slow.Next = tmp.Next
+	tmp.Next = nil
 	return dummy.Next
 }
 
@@ -43,19 +44,30 @@ func removeNthFromEnd2(head *ListNode, n int) *ListNode {
 }
 
 func TestRemoveNthFromEnd(t *testing.T) {
-	list := &ListNode{
-		Val: 1, Next: &ListNode{
-			Val: 2, Next: &ListNode{
-				Val: 3, Next: &ListNode{
-					Val: 4, Next: &ListNode{
-						Val: 5, Next: &ListNode{
-							Val: 6,
-						},
-					},
-				},
-			},
+	tests := []struct {
+		n      int
+		array  []int
+		output []int
+	}{
+		{
+			n:      2,
+			array:  []int{1, 2, 3, 4, 5},
+			output: []int{1, 2, 3, 5},
+		},
+		{
+			n:      1,
+			array:  []int{1},
+			output: []int{},
+		},
+		{
+			n:      1,
+			array:  []int{1, 2},
+			output: []int{1},
 		},
 	}
-	a := removeNthFromEnd(list, 7)
-	fmt.Println(a.String())
+	for _, test := range tests {
+		before := generateListViaArray(test.array)
+		after := removeNthFromEnd(before, test.n)
+		assert.Equal(t, test.output, generateArrayViaList(after))
+	}
 }
