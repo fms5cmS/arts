1cdf8e15bbd00d2f4b65d474f16469b066b6d217
fms5cmS <fms5cms@163.com>
2022-11-03 11:39:02 +0800

    add test for 242

--- algorithm/hashRela/242_isAnagram_test.go
+++ algorithm/hashRela/242_isAnagram_test.go
@@ -1,8 +1,13 @@
 package hashRela
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
 func isAnagram(s string, t string) bool {
 	var record [26]int
-	// recordMyself s 中每个字符出现的次数
+	// record 记录 s 中每个字符出现的次数
 	for _, ss := range s {
 		record[ss-'a']++
 	}
@@ -16,4 +21,26 @@ func isAnagram(s string, t string) bool {
 	}
 	// record 所有元素值均为 0，说明 s 和 t 是字母异位词
 	return true
+}
+
+func TestIsAnagram(t *testing.T) {
+	tests := []struct {
+		s      string
+		t      string
+		result bool
+	}{
+		{
+			s:      "anagram",
+			t:      "nagaram",
+			result: true,
+		},
+		{
+			s:      "rat",
+			t:      "car",
+			result: false,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.result, isAnagram(test.s, test.t))
+	}
 }
