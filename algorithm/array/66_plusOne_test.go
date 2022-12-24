ffacea54ef5754a94e88e700c407fd83ad993877
fms5cmS <fms5cms@163.com>
2022-12-24 09:09:47 +0800

    add test

--- algorithm/array/66_plusOne_test.go
+++ algorithm/array/66_plusOne_test.go
@@ -1,5 +1,10 @@
 package array
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
 func plusOne(digits []int) []int {
 	carry := 1
 	result := make([]int, 0)
@@ -13,4 +18,33 @@ func plusOne(digits []int) []int {
 	}
 	result = append(result, digits...)
 	return result
+}
+
+func TestPlusOne(t *testing.T) {
+	tests := []struct {
+		name   string
+		digits []int
+		want   []int
+	}{
+		{
+			name:   "1",
+			digits: []int{1, 2, 3},
+			want:   []int{1, 2, 4},
+		},
+		{
+			name:   "2",
+			digits: []int{4, 3, 2, 1},
+			want:   []int{4, 3, 2, 2},
+		},
+		{
+			name:   "3",
+			digits: []int{9},
+			want:   []int{1, 0},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.want, plusOne(test.digits))
+		})
+	}
 }
