8f41e29de768ef5279d7d704acfad87b5e9f1ee4
fms5cmS <fms5cms@163.com>
2024-03-01 10:46:51 +0800

    add once note

--- libs/builtin_sync/once_test.go
+++ libs/builtin_sync/once_test.go
@@ -1,7 +1,31 @@
 package builtin_sync
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
 
 func TestOnce(t *testing.T) {
+	var pool any
+	var once sync.Once
+	var initFn = func() {
+		fmt.Println("run...")
+		pool = 1
+	}
+	for i := 0; i < 10; i++ {
+		once.Do(initFn)
+		t.Log(pool)
+	}
+}
 
+func TestOnceValue(t *testing.T) {
+	var initFn = func() any {
+		fmt.Println("run...")
+		return 1
+	}
+	var poolGenerator = sync.OnceValue(initFn)
+	for i := 0; i < 10; i++ {
+		t.Log(poolGenerator())
+	}
 }
